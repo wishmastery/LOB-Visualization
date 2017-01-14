0e7de470b62d13bc06b83646d51b2465deb005e1
Heran Zhang <wishmastery@users.noreply.github.com>
2017-01-14 17:26:27 -0600

    Update hdf5_generation.py

--- hdf5_generation.py
+++ hdf5_generation.py
@@ -8,6 +8,11 @@ num_levels = 10
 column_names = [['ask'+str(i+1)+'p','ask'+str(i+1)+'q','bid'+str(i+1)+'p','bid'+str(i+1)+'q'] for i in range(num_levels)]
 column_names = [names for levels in column_names for names in levels]
 df = pd.read_csv('orderbook.csv', names = column_names, dtype=int)
-df.to_hdf('store.h5', 'book', append=True)
-print (df)
-
+price_names = [['ask'+str(i+1)+'p','bid'+str(i+1)+'p'] for i in range(num_levels)]
+price_names = [names for levels in price_names for names in levels]
+df_prices = df[price_names]
+quantity_names = [['ask'+str(i+1)+'q','bid'+str(i+1)+'q'] for i in range(num_levels)]
+quantity_names = [names for levels in quantity_names for names in levels]
+df_quantity = df[quantity_names]
+df_prices.to_hdf('store.h5', 'prices', append=True)
+df_quantity.to_hdf('store.h5', 'quantity', append=True)
